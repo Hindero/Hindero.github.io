895af82677c5dc0e712c904d9352be5ef8879c9d
Hindero <r0636908@student.luca-arts.be>
2019-03-01 13:33:20 +0100

    nieuw niieuw
    ocoiez

--- script.js
+++ script.js
@@ -5,14 +5,24 @@ $(document).ready(function(){
 //    document.querySelector("#file3").style.display = "none";
     
     //FILE 1
+    $('#intronext').click(function(){
+        document.querySelector("#file1").style.display = "block";
+        document.querySelector("#intro").style.display = "none";
+    });
+    
+    //FILE 1
     $('#file1next').click(function(){
         document.querySelector("#file2").style.display = "block";
+        document.querySelector("#file1").style.display = "none";
+    });
+     $('#file1prev').click(function(){
+        document.querySelector("#intro").style.display = "block";
         document.querySelector("#file1").style.display = "none";
     });
     
     //FILE 2
     $('#file2next').click(function(){
-        document.querySelector("#file3").style.display = "block";
+        document.querySelector("#file-A").style.display = "block";
         document.querySelector("#file2").style.display = "none";
     });
     
@@ -21,26 +31,26 @@ $(document).ready(function(){
         document.querySelector("#file2").style.display = "none";
     });
     
-    //FILE 3
-    $('#file3next').click(function(){
-        document.querySelector("#file-A").style.display = "block";
-        document.querySelector("#file3").style.display = "none";
-    });
-    
-     $('#file3prev').click(function(){
-        document.querySelector("#file2").style.display = "block";
-        document.querySelector("#file3").style.display = "none";
-    });
-    
-    //FILE A
+      //FILE A
     $('#file-Anext').click(function(){
-        document.querySelector("#file4").style.display = "block";
+        document.querySelector("#file3").style.display = "block";
         document.querySelector("#file-A").style.display = "none";
     });
     
      $('#file-Aprev').click(function(){
-        document.querySelector("#file3").style.display = "block";
+        document.querySelector("#file2").style.display = "block";
         document.querySelector("#file-A").style.display = "none";
+    });
+    
+    //FILE 3
+    $('#file3next').click(function(){
+        document.querySelector("#file4").style.display = "block";
+        document.querySelector("#file3").style.display = "none";
+    });
+    
+     $('#file3prev').click(function(){
+        document.querySelector("#file-A").style.display = "block";
+        document.querySelector("#file3").style.display = "none";
     });
     
     //FILE 4
